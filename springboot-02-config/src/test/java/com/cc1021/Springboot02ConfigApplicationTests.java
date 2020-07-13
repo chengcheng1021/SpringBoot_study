4fefa3c03516b45502451ccba6df69b8345082d4
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-13 08:40:28 +0800

    yaml入门实操

--- springboot-02-config/src/test/java/com/cc1021/Springboot02ConfigApplicationTests.java
+++ springboot-02-config/src/test/java/com/cc1021/Springboot02ConfigApplicationTests.java
@@ -1,13 +1,25 @@
 package com.cc1021;
 
+import com.cc1021.pojo.Dog;
+import com.cc1021.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot02ConfigApplicationTests {
 
+    @Autowired
+    private Dog dog;
+    @Autowired
+    private Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(dog);
+
+        System.out.println(person);
     }
 
 }
