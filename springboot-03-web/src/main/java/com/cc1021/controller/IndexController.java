231206c2002a257e4e5cd6a5b61878a346f771f4
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-13 17:36:01 +0800

    首页和icon定制

---
+++ springboot-03-web/src/main/java/com/cc1021/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.cc1021.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 在 templates 目录下的所有页面，只能通过 controller 来跳转！
+ * 这个需要模版引擎的支持！thymeleaf
+ */
+@Controller
+public class IndexController {
+
+//    @RequestMapping("/index")
+//    public String index() {
+//        return "index";
+//    }
+}
