8b6706f0632c438774e76f26fef566258becaa39
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-13 19:03:41 +0800

    thymeleaf语法

--- springboot-03-web/src/main/java/com/cc1021/controller/IndexController.java
+++ springboot-03-web/src/main/java/com/cc1021/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+
 /**
  * 在 templates 目录下的所有页面，只能通过 controller 来跳转！
  * 这个需要模版引擎的支持！thymeleaf
@@ -13,7 +15,9 @@ public class IndexController {
 
     @RequestMapping("/test")
     public String test(Model model) {
-        model.addAttribute("msg", "hello,springboot!");
+        model.addAttribute("msg", "<h1>hello,springboot!</h1>");
+
+        model.addAttribute("users", Arrays.asList("cheng","mali"));
         return "test";
     }
 }
