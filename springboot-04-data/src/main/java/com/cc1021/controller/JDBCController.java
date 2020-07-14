9cdcb80cfaf05c00a0a51886a1801483b2c1a654
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-14 21:51:39 +0800

    JDBC整合使用增删改查

--- springboot-04-data/src/main/java/com/cc1021/controller/JDBCController.java
+++ springboot-04-data/src/main/java/com/cc1021/controller/JDBCController.java
@@ -3,6 +3,7 @@ package com.cc1021.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,9 +20,35 @@ public class JDBCController {
 
     @GetMapping("/userList")
     public List<Map<String, Object>> userList() {
-        String sql = "select * from users";
+        String sql = "select * from account";
 
         List<Map<String, Object>> list_maps = jdbcTemplate.queryForList(sql);
         return list_maps;
     }
+
+    @GetMapping("/addUser")
+    public String addUser() {
+        String sql = "insert into account(id, name, money) values(6,'艳文',1)";
+        jdbcTemplate.update(sql);
+        return "add-ok";
+    }
+
+    @GetMapping("/updateUser/{id}")
+    public String updateUser(@PathVariable("id") int id) {
+        String sql = "update account set name=?, money=? where id="+id;
+        // 封装
+        Object[] objects = new Object[2];
+        objects[0] = "吴艳文";
+        objects[1] = 2;
+        jdbcTemplate.update(sql, objects);
+        return "update-ok";
+    }
+
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        String sql = "delete from account where id=?";
+        jdbcTemplate.update(sql, id);
+        return "delete-ok";
+    }
+
 }
