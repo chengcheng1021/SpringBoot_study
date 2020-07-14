0578f3dbd7f12815a99527450c04f89ae9fdadc9
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-14 11:18:07 +0800

    国际化实操

--- springboot-03-web2/src/main/java/com/cc1021/config/MyMvcConfig.java
+++ springboot-03-web2/src/main/java/com/cc1021/config/MyMvcConfig.java
@@ -1,6 +1,8 @@
 package com.cc1021.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,5 +18,14 @@ public class MyMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+    }
+
+    /**
+     * 自定义国际化组件生效了
+     * @return
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
     }
 }
