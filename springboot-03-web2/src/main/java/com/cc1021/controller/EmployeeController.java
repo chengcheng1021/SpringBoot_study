efd7a6fec121fa9f257f02906ac91d30ff7a2337
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-14 17:57:40 +0800

    修改员工实现

--- springboot-03-web2/src/main/java/com/cc1021/controller/EmployeeController.java
+++ springboot-03-web2/src/main/java/com/cc1021/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.util.StringUtils;
@@ -56,6 +57,35 @@ public class EmployeeController {
         System.out.println(employee);
         // 新增操作
         employeeDao.save(employee);// 调用底层业务方法保存员工信息
+        return "redirect:/emps";
+    }
+
+    /**
+     * 修改员工页面
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("/emp/{id}")
+    public String toUpdateEmp(@PathVariable("id") Integer id, Model model) {
+        // 查出原来的数据
+        Employee employee = employeeDao.getEmployeeById(id);
+        model.addAttribute("emp", employee);
+        // 查询所有的部门的信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments", departments);
+
+        return "emp/update";
+    }
+
+    /**
+     * 修改员工信息
+     * @param employee
+     * @return
+     */
+    @PostMapping("/updateEmp")
+    public String updateEmp(Employee employee) {
+        employeeDao.save(employee);
         return "redirect:/emps";
     }
 }
