479bec68109be5271d30f67a68181ece140714c4
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-15 20:39:33 +0800

    Shiro整合mybatis

--- shiro-springboot/src/main/java/com/cc1021/config/UserRealm.java
+++ shiro-springboot/src/main/java/com/cc1021/config/UserRealm.java
@@ -1,15 +1,21 @@
 package com.cc1021.config;
 
+import com.cc1021.pojo.User;
+import com.cc1021.service.UserService;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * 自定义的 UserRealm
  */
 public class UserRealm extends AuthorizingRealm {
+
+    @Autowired
+    UserService userService;
 
     /**
      * 授权
@@ -33,16 +39,19 @@ public class UserRealm extends AuthorizingRealm {
         System.out.println("执行了=>认证 doGetAuthenticationInfo");
 
         // 用户名，密码～
-        String name = "root";
-        String password = "123456";
+//        String name = "root";
+//        String password = "123456";
+
 
         UsernamePasswordToken userToken = (UsernamePasswordToken) authenticationToken;
-
-        if (!userToken.getUsername().equals(name)) {
-            return null;    // 抛出异常 UnknownAccountException
+        // 连接真实数据库
+        User user = userService.queryUserByName(userToken.getUsername());
+        if (user == null) { // 没有这个人
+            return null; // 抛出异常 UnknownAccountException
         }
 
+        // 可以加密，MD5，MD5盐值加密
         // 密码认证，shiro做
-        return new SimpleAuthenticationInfo("", password, "");
+        return new SimpleAuthenticationInfo("", user.getPwd(), "");
     }
 }
