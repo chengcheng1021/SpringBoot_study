6090ff6e0ec2741203bf787b4f3fccd3ce9bd8c3
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-15 15:54:25 +0800

    记住我以及首页定制

--- springboot-06-security/src/main/java/com/cc1021/config/SecurityConfig.java
+++ springboot-06-security/src/main/java/com/cc1021/config/SecurityConfig.java
@@ -30,12 +30,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/level3/**").hasRole("vip3");
 
         // 没有权限默认会到登录页面，需要开启登录的页面
-        http.formLogin();
+        // 定制登录页面 toLogin
+        http.formLogin().loginPage("/toLogin").usernameParameter("user").passwordParameter("pwd").loginProcessingUrl("/login");
 
         // 注销，开启了注销功能
         // 防止网站工具：get，post
         http.csrf().disable();  // 关闭csrf功能
         http.logout().logoutSuccessUrl("/");
+
+        // 开启记住我功能，cookie保存，默认2周。自定义前端的参数
+        http.rememberMe().rememberMeParameter("remember");
     }
 
     /**
