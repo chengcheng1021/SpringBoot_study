d52c212a0aa7fe48e2fca522b4fd099e19d3bdaf
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-16 19:11:07 +0800

    简单邮件发送

--- springboot-09-test/src/test/java/com/cc1021/Springboot09TestApplicationTests.java
+++ springboot-09-test/src/test/java/com/cc1021/Springboot09TestApplicationTests.java
@@ -1,13 +1,29 @@
 package com.cc1021;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootTest
 class Springboot09TestApplicationTests {
 
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
     @Test
     void contextLoads() {
+
+        // 一个简单的邮件～
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setSubject("小刘侨，你好呀～");
+        mailMessage.setText("谢谢你哦～");
+        mailMessage.setTo("492245711@qq.com");
+        mailMessage.setFrom("492245711@qq.com");
+
+        mailSender.send(mailMessage);
     }
 
 }
